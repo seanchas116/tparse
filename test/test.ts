b602057f08d972be65ab52fdb1986586d636defb
Ryohei Ikegami <iofg2100@gmail.com>
2015-12-17 12:10:42 +0900

    Add map and repeat spec

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,6 @@
+require('source-map-support').install();
 import {assert} from "chai";
-import {Range, Parser, sequence, choose, string, regExp, lazy} from "../src/index";
+import {SyntaxError, Range, Parser, sequence, choose, string, regExp, lazy} from "../src/index";
 
 interface IntegerNode {
   value: number;
@@ -87,5 +88,21 @@ describe("Parser", () => {
     assert.equal(node.range.end.line, 4);
     assert.equal(node.range.end.column, 17);
     assert.equal(node.range.end.index, 43);
+  });
+
+  describe("map", () => {
+    it("maps parsed value", () => {
+      const parser = regExp(/[0-9]/).repeat().text().map(s => parseInt(s, 10));
+      assert.equal(parser.parseString("123"), 123);
+    });
+  });
+  describe("repeat", () => {
+    it("repeats parser", () => {
+      const parser = regExp(/[a-zA-Z]/).repeat(2, 3).text();
+      assert.throw(() => parser.parseString("a"), "[:1:2]: Expected '/[a-zA-Z]/'; found ''");
+      assert.equal(parser.parseString("ab"), "ab");
+      assert.equal(parser.parseString("xyz"), "xyz");
+      assert.throw(() => parser.parseString("hjkl"), "[:1:5]: Expected '[EOS]'; found ''");
+    });
   });
 });
